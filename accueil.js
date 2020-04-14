45eb6d5efe82e0f79e510be850ee33c0b11bbb5c
Veljko <kovacevic99@gmail.com>
2020-04-14 23:31:26 +0200

    Transit to Boistrap 4 page d'accueil

--- accueil.js
+++ accueil.js
@@ -1,15 +1,26 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+$(document).ready(function () {
+    $('.header').height($(window).width()*0.35);
   });
+
+  $(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        slidesPerGroup: 4,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          renderBullet: function (index, className) {
+          return '<span class="' + className + '"></span>';
+        },
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+  })
+
+  
--- nav.php
+++ nav.php
@@ -1,28 +1,41 @@
-<!DOCTYPE html>
-<html>
-
-<nav class="navbar navbar-inverse">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
-        <img src="Image/logoebayece.jpg" class="navbar-brand" style="width: 150px; height:100%; margin-top:-10px;" href="#">
-      </div>
-      <div class="collapse navbar-collapse" id="myNavbar">
-        <ul class="nav navbar-nav">
-          <li class="active"><a href="#">Home</a></li>
-          <li ><a class="nav-link"href="#">About</a></li>
-          <li class="nav-link"><a href="#">Projects</a></li>
-          <li class="nav-link"><a href="#">Contact</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-</html>
+<nav class="navbar navbar-expand-lg navbar-light sticky-top">
+  <a class="navbar-brand" href="#">
+    <img src="Image/minilogoebayece.jpg" width="30" height="30" class="d-inline-block" alt="">
+    EBAY ECE
+  </a>
+  
+  
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item hover-link">
+        <a class="nav-link hover-link" href="#">Catégories</a>
+      </li>
+      <li class="nav-item hover-link">
+        <a class="nav-link" href="#">Vendre</a>
+      </li>
+      <li class="nav-item hover-link">
+        <a class="nav-link" href="#">A Propos</a>
+      </li>
+      <li class="nav-item hover-link">
+        <a class="nav-link" href="#">Vendre</a>
+      </li>
+    </ul>
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <form class="form-inline">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </li>
+      <li class="nav-item hover-link">
+        <a href="#" class="nav-link"><i class="fas fa-sign-in-alt"></i> Login</a>
+      </li>
+    </ul>
+  </div>
+</nav>
